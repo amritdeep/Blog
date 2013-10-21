4bc6a89285285ee99e5ea1d27f1783428e00d186
amritdeep <amritdeepdhungana@hotmail.com>
2013-10-21 12:15:27 +0545

    add some template

--- Blog/settings.py
+++ Blog/settings.py
@@ -108,6 +108,7 @@ ROOT_URLCONF = 'Blog.urls'
 WSGI_APPLICATION = 'Blog.wsgi.application'
 
 TEMPLATE_DIRS = (
+    '/home/amrit/venv/Blog/templates',
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
--- Blog/urls.py
+++ Blog/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from article.views import HelloTemplate
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -6,6 +7,10 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     # Examples:
+    url(r'^hello/$', 'article.views.hello', name='hello'),
+    url(r'^hello_template/$', 'article.views.hello_template', name='hello_template'),
+    url(r'^hello_template_simple/$', 'article.views.hello_template_simple', name="hello_template_simple"),
+    url(r'hello_class_view/$', HelloTemplate.as_view()),
     # url(r'^$', 'Blog.views.home', name='home'),
     # url(r'^Blog/', include('Blog.foo.urls')),
 
