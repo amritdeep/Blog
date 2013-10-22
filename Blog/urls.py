0f4b78c9f0de04bfdab92c4785161907b5a9ceed
amritdeep <amritdeepdhungana@hotmail.com>
2013-10-22 10:31:27 +0545

    add user login and lougout function

--- Blog/urls.py
+++ Blog/urls.py
@@ -9,7 +9,6 @@ urlpatterns = patterns('',
     # Examples:
     (r'^articles/', include('article.urls')),
 
-
     # url(r'^hello/$', 'article.views.hello', name='hello'),
     # url(r'^hello_template/$', 'article.views.hello_template', name='hello_template'),
     # url(r'^hello_template_simple/$', 'article.views.hello_template_simple', name="hello_template_simple"),
@@ -23,4 +22,11 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+
+    # user auth urls
+    url(r'^accounts/login/$', 'Blog.views.login'),
+    url(r'^accounts/auth/$', 'Blog.views.auth_view'),
+    url(r'^accounts/logout/$', 'Blog.views.logout'),
+    url(r'^accounts/loggedin/$', 'Blog.views.loggedin'),
+    url(r'^accounts/invalid/$', 'Blog.views.invalid_login'),
 )
